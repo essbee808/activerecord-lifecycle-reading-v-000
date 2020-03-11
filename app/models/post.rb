84e26704ed0c21363750595adf9d3aa7cd6b62da
Sheena Bonilla <sheena.myb@gmail.com>
2020-03-11 18:59:10 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,8 @@ class Post < ActiveRecord::Base
   # use before_validation when modifying an attribute of the model
   # any other action => use before_save
 
+  before_save :email_author_about_post
+  
   private
 
   def is_title_case
